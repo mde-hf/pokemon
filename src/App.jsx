bd847bf32ab89facb2dc6ddbefab21ec83f12807
Mohan <mde@hellofresh.com>
2025-02-06 09:57:19 +0100

    inital setup with tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -12,18 +10,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="bg-blue-500 text-white p-4">Hello, Tailwind!</div>
+
     </>
   )
 }
