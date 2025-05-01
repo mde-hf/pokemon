53af9ca30e019c6a007955d11793f9bc01c78fa7
Mohan <mde@hellofresh.com>
2025-05-01 21:30:45 +0200

    search bar and pokemon data handling

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import getPokemon from "../api/pokemonApi";
+import { data } from "autoprefixer";
 
 function DetailPage() {
+
+
+    const [pokemonName, setPokemonName] = useState("")
+
+    const [pokemonData, setPokemonData] = useState([])
+
+    async function handleSearch() {
+
+        const result = await getPokemon(pokemonName)
+        if (!result) {
+            console.log("Pokemon not found")
+        } else {
+            setPokemonData(result)
+
+        }
+    }
+
+
+
+    const getRandomPokemon = () => { }
 
     return (
 
         <div>
 
-            <h1>Detail Page</h1>
+            <h1>Search Pokemon Details</h1>
+            <SearchBar
+                pokemonName={pokemonName}
+                setPokemonName={setPokemonName}
+                handleSearch={handleSearch}
+                getRandomPokemon={getRandomPokemon}
+            />
 
         </div >
     )
